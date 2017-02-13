374190653038918e7c9075b28da015c116f121d2
anonymous0010 <ctoxtli@gmail.com>
2017-02-13 10:19:21 -0500

    New changes

--- automaton/js/MessengerListener.js
+++ automaton/js/MessengerListener.js
@@ -5,7 +5,7 @@ if (!window.tox) {
 window.tox.lastMessage = '';
 
 tox.requestMessages = function() {
-	var url = 'https://www.linkedin.com/chrome/inbox/activity/notifications/v2?rnd=' + (new Date()).getTime();
+	var url = 'https://www.linkedin.com/messaging/conversationsView?includeSent=true&clearUnseen=false&after=' + (new Date()).getTime();
 	$.get(url, function(data) {
 		tox.lastMessage = data;
 		console.log(data);
