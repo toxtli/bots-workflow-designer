9df03af2d12a3fb0f4c513ab410c181ad18ada99
anonymous0010 <ctoxtli@gmail.com>
2017-02-12 19:24:04 -0500

    New changes

--- automaton/modules/MessengerListenerModule.py
+++ automaton/modules/MessengerListenerModule.py
@@ -8,7 +8,7 @@ class MessengerListenerModule(Module):
 	DATABASE_TABLE = 'messages'
 
 	def run(self, params, callback):
-		self.MAX_PROCESSES = 1
+		self.MAX_PROCESSES = 10
 		self.push(params, callback, self.run_queue)
 		# super(self.__class__, self).run(params, callback)
 
@@ -16,13 +16,6 @@ class MessengerListenerModule(Module):
 		# https://www.linkedin.com/messaging/conversationsView?includeSent=true&clearUnseen=false&after=1486408991388
 		LogHelper.log('EXECUTING ' + self.__class__.__name__, True)
 		LogHelper.log('INPUT ' + self.__class__.__name__ + ' ' + str(params))
-		self.db = DatabaseHelper(table=self.DATABASE_TABLE)
-		sel = LinkedinHelper.clone_driver(params['bots']['driver'])
-		args = {'driver': sel}
-		messenger_listener = MessengerListener(args)
-		def messenger_listener_handler(message):
-			self.db.insert_one(message)
-			output = {'results': message}
-			LogHelper.log('OUTPUT ' + self.__class__.__name__ + ' ' + str(output))
-			self.pop(params, output, callback)
-		messenger_listener.run(params, messenger_listener_handler)
+		output = {'results':[]}
+		LogHelper.log('OUTPUT ' + self.__class__.__name__ + ' ' + str(output))
+		self.pop(params, output, callback)
